bdbd502692d6359fdfe393ca7fb451b422d572f6
Wojciech_Bladek <wojciechbladekk@gmail.com>
2023-07-11 16:15:22 +0200

    add public email and password for reuse

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./playwright/tests",
+  testDir: "./Playwright/tests",
   /* Maximum time one test can run for. */
   timeout: 20 * 1000,
   expect: {
