7b9f1bb2e2c4a395d19232850bc9a6c74f22604e
Wojciech_Bladek <wojciechbladekk@gmail.com>
2023-07-11 16:31:48 +0200

    Features

--- Playwright/pages/login.page.ts
+++ Playwright/pages/login.page.ts
@@ -1,16 +1,36 @@
 import { Locator, Page } from "@playwright/test";
 import fs from "fs";
 
-const loginDataFile = fs.readFileSync("playwright.env.json", {
-  encoding: "utf8",
-});
-const loginData = JSON.parse(loginDataFile);
-
+const loginDataFile = () => {
+  try {
+    const loginDataFile = fs.readFileSync("playwright.env.json", {
+      encoding: "utf8",
+    });
+    if (loginDataFile) {
+      const loginData = JSON.parse(loginDataFile);
+      return loginData;
+    }
+  } catch (error) {
+    console.log(`Error private content: ${error}`);
+  }
+};
 export class LoginPage {
   constructor(private page: Page) {}
 
-  email: string = loginData.user.email;
-  password: string = loginData.user.password;
+  email() {
+    if (loginDataFile()) {
+      const email: string = loginDataFile().user.email;
+      return email;
+    }
+  }
+  password() {
+    if (loginDataFile()) {
+      const password: string = loginDataFile().user.password;
+      return password;
+    }
+  }
+  // email: string = loginDataFile().user.email;
+  // password: string = loginDataFile().user.password;
 
   publicEmail: string = "test.demo@gmail.com";
   publicPassword: string = "DemoTest12345@!";
@@ -39,21 +59,21 @@ export class LoginPage {
   }
 
   useEmail() {
-    if (this.email) {
-      return this.email;
+    if (this.email()) {
+      return this.email();
     } else {
       return this.publicEmail;
     }
   }
   usePassword() {
-    if (this.password) {
-      return this.password;
+    if (this.password()) {
+      return this.password();
     } else {
       return this.publicPassword;
     }
   }
   useExceptedUserName() {
-    if (this.email) {
+    if (this.email()) {
       return this.exceptedUserName;
     } else {
       return "test.demo";
