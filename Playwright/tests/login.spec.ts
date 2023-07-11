ed3fb89b716b5462b71e0a25dac96611a7810c41
WojciechBladek <109952619+WojciechBladek@users.noreply.github.com>
2023-07-11 14:57:27 +0200

    Feature/e2e (#7)
    * add helpers to playwright and new model
    
    * create loginPage tests
    
    * Update README.md (#2)
    
    * Create README.md (#3)
    
    * Update README.md (#4)
    
    * Create pull_request_template.md (#5)
    
    * Update pull_request_template.md (#6)
    
    * create loginPage tests

---
+++ Playwright/tests/login.spec.ts
@@ -0,0 +1,56 @@
+import { test, expect, Page } from "@playwright/test";
+import { LoginPage } from "../pages/login.page";
+
+test.describe("Login to the application", () => {
+  let loginPage: LoginPage;
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    await page.goto("/my-account", { waitUntil: "networkidle" });
+  });
+
+  test("Login with correct username and password", async ({ page }) => {
+    // ARRANGE
+    await loginPage.loginToApp(loginPage.email, loginPage.password);
+
+    // ASSERT
+    expect(await loginPage.loginConfirm.textContent()).toEqual(
+      `Hello ${loginPage.exceptedUserName} (not ${loginPage.exceptedUserName}? Log out)`
+    );
+  });
+
+  test("Try login without username and password", async ({ page }) => {
+    // ARRANGE
+    await loginPage.loginToApp();
+
+    // ASSERT
+    expect(await loginPage.loginError.textContent()).toEqual(
+      "Error: Username is required."
+    );
+  });
+
+  test("Try login with incorrect Username", async ({ page }) => {
+    //ACT
+    const incorrectUserName = "Test12345@gm";
+
+    // ARRANGE
+    await loginPage.loginToApp(incorrectUserName, loginPage.password);
+
+    // ASSERT
+    expect(await loginPage.loginError.textContent()).toEqual(
+      `Error: The username ${incorrectUserName} is not registered on this site. If you are unsure of your username, try your email address instead.`
+    );
+  });
+
+  test("Try login with incorrect Password", async ({ page }) => {
+    //ACT
+    const incorrectPassword = "Test123@3!!@3";
+
+    // ARRANGE
+    await loginPage.loginToApp(loginPage.email, incorrectPassword);
+
+    // ASSERT
+    expect(await loginPage.loginError.textContent()).toEqual(
+      `Error: The password you entered for the username ${loginPage.email} is incorrect. Lost your password?`
+    );
+  });
+});
---
+++ SoftwareTesting/print-files.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const { readFileSync } = fs;
+
+const readTestCase = readFileSync("./README-test-case.md", {
+  encoding: "utf8",
+});
+const readBugReport = readFileSync("./README-bug-report.md", {
+  encoding: "utf8",
+});
+console.log(readTestCase);
+console.log(readBugReport);
