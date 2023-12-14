5b4ef2fccc541bf975858af455ebbda35f4ebf09
WojciechBladek <109952619+WojciechBladek@users.noreply.github.com>
2023-12-14 14:42:34 +0100

    Update gmail-test.spec.ts

--- playwright-main/tests/gmail-test.spec.ts
+++ playwright-main/tests/gmail-test.spec.ts
@@ -1,6 +1,6 @@
 import config from '@_config/email-config';
 import { expect, test } from '@_playwright-main/fixtures/merge.fixture';
-import * as gmail from 'gmail-tester';
+import { get_messages, refresh_access_token } from 'gmail-tester';
 
 const extractUrlFromATag = (input: string): string => {
   const pattern = /<a href="(.*?)"/;
@@ -8,12 +8,12 @@ const extractUrlFromATag = (input: string): string => {
   return match ? match[1] : '';
 };
 
-test.fixme('has email with magic link', async () => {
+test('has email with magic link', async () => {
   const exceptedMessage = 'https://magic-link/delivered';
 
-  gmail.refresh_access_token(config.credentialsPath, config.tokenPath);
+  refresh_access_token(config.credentialsPath, config.tokenPath);
 
-  const emailTest = gmail.get_messages(
+  const emailTest = get_messages(
     config.credentialsPath,
     config.tokenPath,
     config.options,
