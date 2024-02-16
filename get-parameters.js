595c10c39fd37d2f60d67a3c2905ce59447c8b59
Wojciech <wojciechbladekk@gmail.com>
2024-02-16 10:18:36 +0100

    add 2 new methods to map parameters from string

--- get-parameters.js
+++ get-parameters.js
@@ -2,11 +2,11 @@
 // get parameters from string and map it.
 
 const text =
-  "customs.com/resource?firstParameter=test1&secondParameter=test2&thirdParameter=test3";
+  'customs.com/resource?firstParameter=test1&secondParameter=test2&thirdParameter=test3';
 
-const textArray = text.split("?")[1].split("&");
+const textArray = text.split('?')[1].split('&');
 const parameters = textArray.map((param) => {
-  const keyValueArray = param.split("=");
+  const keyValueArray = param.split('=');
   const key = keyValueArray[0];
   const value = keyValueArray[1];
   return { [key]: value };
@@ -19,3 +19,21 @@ console.log(parameters);
 //   { secondParameter: 'test2' },
 //   { thirdParameter: 'test3' }
 // ]
+
+// Method 2
+// same output but shorter code.
+const parameters2 = textArray.map((param) => {
+  const [key, value] = param.split('=');
+  return { [key]: value };
+});
+
+console.log(parameters2);
+
+// Method 3
+const parameters3 = textArray.map((param) => {
+  const obj = {};
+  const [key, value] = param.split('=');
+  obj[key] = value;
+  return obj;
+});
+console.log(parameters3);
